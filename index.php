5fd2f079b16f5f6b86f37370fd6f9f9fb474feb9
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-06-25 17:51:58 +0200

    ADD php logic

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
+
+    class Genre{
+
+        public $nome;
+
+        function __construct($_nome){
+
+            $this->nome = $_nome;
+        }
+    }
+
     class Moovie{
 
-        public nome;
-        public anno;
-        public lingua;
+        public $nome;
+        public $anno;
+        public $lingua;
 
-        function __construct($_nome,$_anno,$_lingua){
+        function __construct($_nome,$_anno,$_lingua, Genre $genere){
 
             $this->nome = $_nome;
             $this->anno = $_anno;
             $this->lingua = $_lingua;
 
-        };
+        }
 
         public function isItalian() {
 
@@ -20,16 +31,26 @@
             } else {
             return false;
             }
-        };
+        }
 
 
 
-    };
+    }
+
+    $azione = new Genre("Azione");
+    $fantasy = new Genre("Fantasy");
+    $azione = new Genre("Commedia");
+
     
-    $batman = new Moovie("Batman", 2002, "ING");
-    $superman = new Moovie("Superman", 2005, "ING");
+    $batman = new Moovie("Batman", 2002, "ING", $azione);
+    $superman = new Moovie("Superman", 2005, "ING", $fantasy);
+    $treUomini = new Moovie("Tre Uomini e Una Gamba", 2007, "ITA", $commedia);
+
 
     
+    
+    var_dump($batman->isItalian())
+
 ?>
 
 <!DOCTYPE html>
